3e9926a3ec174b9ecaa9b165c7401d84ee7a924a
Mahdia-toubal <mahdia.toubal@gmail.com>
2023-10-30 20:46:41 +0000

    obj 7 done

--- 7thObjective.php
+++ 7thObjective.php
@@ -8,6 +8,54 @@
         window.history.replaceState( null, null, window.location.href );
     }
 </script>
+<style>
+        body {
+            background-color: beige;
+            font-family: Arial, sans-serif;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: skyblue;
+            border-radius: 5px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+            color: #333;
+            font-size: 14px; /* Adjust the font size as per your requirement */
+        }
+
+        input[type="text"] {
+            width: calc(100% - 20px);
+            padding: 10px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+
+        input[type="submit"] {
+            width: 100%;
+            background-color: beige;
+            color: #333;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #f5f5f5;
+        }
+
+        /* Add some spacing between the forms */
+        form + form {
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
     
@@ -70,11 +118,7 @@
 </body>
 </html><?php
 session_start();
-include("connexionBD.php");
 include("Addfeature.php");
-//include("AddSmartphone.php");
-
-
-
+include("AddSmartphone.php");
 
 ?>
--- Addfeature.php
+++ Addfeature.php
@@ -1,12 +1,9 @@
 <?php
-echo '<form method="POST">';
-echo '<label for="feature">Feature:</label>
-<input type="text" id="feature" name="feature" /><br><br>';
+include("connexionBD.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $feature =  $_POST['feature'];
-    
-}
+echo '<form method="POST">';
+echo '<label for="feature">Feature:</label>';
+echo '<input type="text" id="feature" name="feature" /><br><br>';
 $qtf="SELECT * FROM smartphone";
 
 $features = [];
@@ -15,49 +12,49 @@ foreach($c->query($qtf) as $row) {
     $smartphoneId = $row["Id_smartphone"];
     echo '<label for="feature_'.$smartphoneId.'">'.$row["Name_smartphone"].'</label>';
     echo '<input type="text" id="feature_'.$smartphoneId.'" name="feature_'.$smartphoneId.'"><br><br>';
-    
+
     if(isset($_POST['feature_'.$smartphoneId])) {
         $features[$smartphoneId] = $_POST['feature_'.$smartphoneId];
     }
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $stmt = $c->prepare("INSERT INTO features (Name_Features) VALUES (?)");
-    $stmt->bindParam(1, $feature);
-    $stmt->execute();
-    $feature_id = $c->lastInsertId();
-    
-    $sqlr = "SELECT COUNT(*) as total FROM smartphone";
-    $result = $c->query($sqlr);
-    $row = $result->fetch(PDO::FETCH_ASSOC);
-    $smartphoneCount = $row['total'];
-    
-    
-
-    $sqlr2 = "SELECT * FROM smartphone";
-    $phones = $c->query($sqlr2);
-    $ids = [];
-    while ($row = $phones->fetch(PDO::FETCH_ASSOC)) {
-        array_push($ids, $row["Id_smartphone"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST['feature']) && $_POST['feature']!="") {
+        $feature =  $_POST['feature'];
         
-    };
-    
-    for ($i = 0; $i < (int)$smartphoneCount; $i++) {
+        $stmt = $c->prepare("INSERT INTO features (Name_Features) VALUES (?)");
+        $stmt->bindParam(1, $feature);
+        $stmt->execute();
+        $feature_id = $c->lastInsertId();
         
-        $smartphoneId = $ids[$i];
+        $sqlr = "SELECT COUNT(*) as total FROM smartphone";
+        $result = $c->query($sqlr);
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+        $smartphoneCount = $row['total'];
+        
+        $sqlr2 = "SELECT * FROM smartphone";
+        $phones = $c->query($sqlr2);
+        $ids = [];
+        while ($row = $phones->fetch(PDO::FETCH_ASSOC)) {
+            array_push($ids, $row["Id_smartphone"]);
+        };
+        
+        for ($i = 0; $i < (int)$smartphoneCount; $i++) {
+            $smartphoneId = $ids[$i];
             $valueSmartphoneFeatures = $features[$smartphoneId];
-    
+            
             $sqlInsert = "INSERT INTO smartphone_features (Value_Smartphone_Features, Id_smartphone, Id_Features) 
                 VALUES (:valueSmartphoneFeatures, :smartphoneId, :idFeatures)";
-    
+            
             $stmt = $c->prepare($sqlInsert);
             $stmt->bindParam(':valueSmartphoneFeatures', $valueSmartphoneFeatures);
             $stmt->bindParam(':smartphoneId', $smartphoneId);
             $stmt->bindParam(':idFeatures', $feature_id); 
-    
             $stmt->execute();
         }
+
+        echo 'Feature added successfully!';
+    } 
 }
 echo ' <input type="submit" value="Submit" >';
-    echo '</form>';
-
-    ?> 
+echo '</form>';
+?>
